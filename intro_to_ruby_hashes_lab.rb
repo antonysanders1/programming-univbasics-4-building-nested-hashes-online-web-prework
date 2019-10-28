e6f711cbae7ffd75e63eb20abf03a7626c47705e
Antony Sanders <antony.devon92@gmail.com>
2019-10-28 15:21:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,6 +26,7 @@ monoply = {:railroads =>
  	  :rent_in_dollars => 
      	  {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200
      	  }
+     	  
  	  }
 }
  	
